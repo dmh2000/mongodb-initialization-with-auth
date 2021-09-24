7604223b4a1c05a5fb741800f1128ccab12efc16
dmh2000 <dmh2000@gmail.com>
2021-09-24 10:12:20 -0700

    removed example password from setenv.sh. wasn't being used anywhere anyway

--- setenv.sh
+++ setenv.sh
@@ -1,9 +1,9 @@
 #source this file
 export MONGO_INITDB_ROOT_USERNAME=admin
-export MONGO_INITDB_ROOT_PASSWORD=cLtDFY5JZ7Bc22JV
+export MONGO_INITDB_ROOT_PASSWORD=<set password>
 export MONGO_DB_PATH=/home/dmh2000/projects/mongo/data
 export MONGO_DB_USERNAME=retro
-export MONGO_DB_PASSWORD=EVwmy8zahzNhWuF4
+export MONGO_DB_PASSWORD=<set password>
 export MONGO_DB_DATABASE=retro
 export MONGO_DB_CONTAINER_NAME=mongo_db
 printenv | grep MONGO 
